70275a0939a36ea2fa89bc2da22c26c6ff8e0254
Subject-314159 <168562337+Subject-314159@users.noreply.github.com>
2026-01-23 12:37:37 +0100

    Update const.lua

--- lib/const.lua
+++ lib/const.lua
@@ -6,33 +6,33 @@ const.categories = {
     essential = {}
     -- inserters = {
     --     research_effects = {"inserter-stack-size-bonus", "bulk-inserter-capacity-bonus", "belt-stack-size-bonus"},
-    --     prototypes = {"inserter"}
+    --     research_prototypes = {"inserter"}
     -- },
     -- belts = {
-    --     prototypes = {"linked-belt", "transport-belt", "underground-belt", "lane-splitter", "splitter",
+    --     research_prototypes = {"linked-belt", "transport-belt", "underground-belt", "lane-splitter", "splitter",
     --                   "belt-stack-size-bonus"}
     -- },
     -- crafting_machines = {
-    --     prototypes = {"assembling-machine", "furnace", "rocket-silo"}
+    --     research_prototypes = {"assembling-machine", "furnace", "rocket-silo"}
     -- },
     -- research = {
     --     research_effects = {"laboratory-speed", "laboratory-productivity"},
-    --     prototypes = {"lab", "tool"} -- TODO: Do not look at prototype tool but get all science items dynamically
+    --     research_prototypes = {"lab", "tool"} -- TODO: Do not look at prototype tool but get all science items dynamically
     -- },
     -- logistics = {
     --     research_effects = {"character-logistic-trash-slots", "worker-robot-speed", "worker-robot-storage",
     --                         "max-failed-attempts-per-tick-per-construction-queue",
     --                         "max-successful-attempts-per-tick-per-construction-queue", "worker-robot-battery",
     --                         "character-logistic-requests", "vehicle-logistics"},
-    --     prototypes = {"logistic-robot", "construction-robot", "roboport", "roboport-equipment"}
+    --     research_prototypes = {"logistic-robot", "construction-robot", "roboport", "roboport-equipment"}
     -- },
     -- combat = {
     --     research_effects = {"turret-attack", "ammo-damage", "gun-speed", "artillery-range"},
-    --     prototypes = {"active-defense-equipment", "ammo-turret", "artillery-turret", "electric-turret", "fluid-turret", "ammo", "capsule", "gun"}
+    --     research_prototypes = {"active-defense-equipment", "ammo-turret", "artillery-turret", "electric-turret", "fluid-turret", "ammo", "capsule", "gun"}
     -- },
     -- combat_robots = {
     --     research_effects = {"maximum-following-robots-count", "follower-robot-lifetime"},
-    --     prototypes = {"combat-robot"}
+    --     research_prototypes = {"combat-robot"}
     -- },
     -- character_bonus = {
     --     research_effects = {"character-logistic-trash-slots", "give-item", "character-crafting-speed",
@@ -41,34 +41,34 @@ const.categories = {
     --                         "character-resource-reach-distance", "character-item-pickup-distance",
     --                         "character-loot-pickup-distance", "character-inventory-slots-bonus",
     --                         "character-health-bonus", "character-logistic-requests"},
-    --     prototypes = {"roboport-equipment", "active-defense-equipment", "battery-equipment", "belt-immunity-equipment",
+    --     research_prototypes = {"roboport-equipment", "active-defense-equipment", "battery-equipment", "belt-immunity-equipment",
     --                   "energy-shield-equipment", "equipment-ghost", "generator-equipment", "inventory-bonus-equipment",
     --                   "movement-bonus-equipment", "night-vision-equipment", "solar-panel-equipment"}
     -- },
     -- recipes = {
     --     research_effects = {"unlock-recipe"},
-    --     prototypes = {"recipe"}
+    --     research_prototypes = {"recipe"}
     -- },
     -- effect_modifiers = {
     --     research_effects = {"mining-drill-productivity-bonus", "laboratory-productivity", "change-recipe-productivity",
     --                         "unlock-quality", "laboratory-speed", "laboratory-productivity"},
-    --     prototypes = {"beacon", "module"}
+    --     research_prototypes = {"beacon", "module"}
     -- },
     -- vehicles = {
     --     research_effects = {"train-braking-force-bonus", "rail-support-on-deep-oil-ocean",
     --                         "rail-planner-allow-elevated-rails"},
-    --     prototypes = {"locomotive", "artillery-wagon", "cargo-wagon", "infinity-cargo-wagon", "fluid-wagon", "car",
+    --     research_prototypes = {"locomotive", "artillery-wagon", "cargo-wagon", "infinity-cargo-wagon", "fluid-wagon", "car",
     --                   "spider-vehicle", "spidertron-remote"}
     -- },
     -- space = {
     --     research_effects = {"unlock-space-location", "unlock-space-platforms"},
-    --     prototypes = {"space-location", "cargo-bay", "rocket-silo", "space-platform-hub", "space-platform-starter-pack",
+    --     research_prototypes = {"space-location", "cargo-bay", "rocket-silo", "space-platform-hub", "space-platform-starter-pack",
     --                   "thruster", "asteroid-collector", "cargo-landing-pad", "cargo-landing-pad-count"}
     -- },
     -- misc = {
     --     research_effects = {"deconstruction-time-to-live", "nothing", "beacon-distribution", "unlock-circuit-network",
     --                         "uranium-mining", "cliff-deconstruction-enabled"},
-    --     prototypes = {"blueprint-book", "selection-tool", "blueprint", "copy-paste-tool", "deconstruction-item", "upgrade-item"}
+    --     research_prototypes = {"blueprint-book", "selection-tool", "blueprint", "copy-paste-tool", "deconstruction-item", "upgrade-item"}
     -- }
 }
 
