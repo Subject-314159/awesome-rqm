f6532ae40d8c22ed7c3956d730536ba27dbdc6b6
Subject-314159 <168562337+Subject-314159@users.noreply.github.com>
2026-01-26 09:25:21 +0100

    wip

--- lib/state/tech.lua
+++ lib/state/tech.lua
@@ -305,7 +305,8 @@ local get_unresearched_blocking_technologies = function(force_index)
     for _, tech in pairs(f.technologies) do
         local et = env[tech.name]
         if et and et.is_blocking and not tech.researched then
-            table.insert(arr, tech.name)
+            -- table.insert(arr, tech.name)
+            arr[tech.name] = true
         end
     end
     return arr
@@ -319,7 +320,8 @@ local get_unresearched_disabled_technologies = function(force_index)
     for _, tech in pairs(f.technologies) do
         local et = env[tech.name]
         if et and (et.hidden or not tech.enabled) and not tech.researched then
-            table.insert(arr, tech.name)
+            -- table.insert(arr, tech.name)
+            arr[tech.name] = true
         end
     end
     return arr
@@ -363,6 +365,10 @@ local get_tech_meta = function(force_index)
         ::continue::
     end
 
+    -- Get all blocking/disabled tech
+    local blocking_tech = get_unresearched_blocking_technologies(force_index)
+    local disabled_tech = get_unresearched_disabled_technologies(force_index)
+
     local arr = {}
     for name, tech in pairs(f.technologies) do
         local et = env[tech.name]
@@ -373,8 +379,9 @@ local get_tech_meta = function(force_index)
             is_disabled = et.hidden or not tech.enabled,
             is_infinite = et.is_infinite,
             is_inherited = (queued_prerequisites[name] and not tech.researched),
-            all_prerequisites = et.all_prerequisites,
-            blocking_prerequisites = et.blocking_prerequisites
+            all_prerequisites = util.deepcopy(et.all_prerequisites), -- TODO figure out if deepcopy is necessary
+            blocking_prerequisites = util.deepcopy(et.blocking_prerequisites), -- TODO figure out if deepcopy is necessary
+            sciences = util.deepcopy(et.sciences)
         }
 
         -- Remove researched (blocking) prerequisites
@@ -550,21 +557,112 @@ local tech_matches_search_text = function(player_index, tech)
     return false
 end
 
+-- stech.get_filtered_technologies_player = function(player_index, filter)
+--     -- Get Storage Force and sfName
+--     local p = game.get_player(player_index)
+--     local f = p.force
+--     local ssf = get_force(f.index)
+--     local ssft = ssf.technology
+
+--     local techlist = stech.get_unresearched_technologies_ordered(f.index)
+--     local filtered_tech = {}
+--     game.print(serpent.line(techlist))
+
+--     for _, tech in pairs(techlist) do
+--         local ssftt = ssft[tech]
+--         if not filter then
+--             goto skip_filter
+--         end
+
+--         -- Filter 0: Search text (do this one first because it will filter out the most sciences)
+--         if filter.search_text and filter.search_text ~= "" and not tech_matches_search_text(player_index, tech) then
+--             goto continue
+--         end
+
+--         -- Filter 1: Matches required sciences (do this one second because it will filter out a lot of sciences)
+--         if #filter.allowed_sciences > 0 and
+--             not util.array_has_all_values(filter.allowed_sciences, (ssftt.sciences or {})) then
+--             goto continue
+--         end
+
+--         -- Filter 2: Disabled/hidden tech
+--         if filter.hide_tech["disabled_tech"] and (not ssftt.technology.enabled or ssftt.hidden) then
+--             goto continue
+--         end
+
+--         -- Filter 3: Manual trigger tech
+--         if filter.hide_tech["manual_trigger_tech"] and ssftt.has_trigger then
+--             goto continue
+--         end
+
+--         -- Filter 4: Infinite tech
+--         if filter.hide_tech["infinite_tech"] and ssftt.is_infinite then
+--             goto continue
+--         end
+
+--         -- Filter 5: Inherited tech
+--         if filter.hide_tech["inherited_tech"] and (ssftt.inherited_by ~= nil or ssftt.queued) then
+--             goto continue
+--         end
+
+--         -- Filter 6: Unavailable successors
+--         if filter.hide_tech["unavailable_successors"] and (ssftt.blocked_by ~= nil or ssftt.disabled_by ~= nil) then
+--             goto continue
+--         end
+
+--         -- Filter 7: Show category
+--         if filter.show_tech ~= "all" then
+--             if filter.show_tech == "essential" then
+--                 if not ssftt.essential then
+--                     goto continue
+--                 end
+--             else
+--                 -- Check if any of this category's prototypes or effects match any of the given tech's prototypes or effects
+--                 for type, prop in pairs(const.categories[filter.show_tech]) do
+--                     if ssftt[type] then
+--                         for _, p in pairs(prop) do
+--                             if ssftt[type][p] then
+--                                 -- There is a match, no need to look further
+--                                 goto skip_filter
+--                             end
+--                         end
+--                     end
+--                 end
+--                 goto continue
+--             end
+--         end
+
+--         ::skip_filter::
+--         -- If we passed all the filters, add the science to our return array
+--         table.insert(filtered_tech, tech)
+
+--         ::continue::
+--     end
+
+--     return filtered_tech
+
+-- end
+
 stech.get_filtered_technologies_player = function(player_index, filter)
     -- Get Storage Force and sfName
     local p = game.get_player(player_index)
     local f = p.force
-    local ssf = get_force(f.index)
-    local ssft = ssf.technology
+    local meta = get_tech_meta(f.index)
+    local env = get_env()
+    -- local ssf = get_force(f.index)
+    -- local ssft = ssf.technology
 
     local techlist = stech.get_unresearched_technologies_ordered(f.index)
     local filtered_tech = {}
-    game.print(serpent.line(techlist))
 
     for _, tech in pairs(techlist) do
-        local ssftt = ssft[tech]
+        -- local ssftt = ssft[tech]
+        -- Show this tech if there is no filter or skip this tech if we don't have metadata
         if not filter then
             goto skip_filter
+        end
+        if not meta[tech] or not env[tech] then
+            goto continue
         end
 
         -- Filter 0: Search text (do this one first because it will filter out the most sciences)
@@ -574,27 +672,27 @@ stech.get_filtered_technologies_player = function(player_index, filter)
 
         -- Filter 1: Matches required sciences (do this one second because it will filter out a lot of sciences)
         if #filter.allowed_sciences > 0 and
-            not util.array_has_all_values(filter.allowed_sciences, (ssftt.sciences or {})) then
+            not util.array_has_all_values(filter.allowed_sciences, (env[tech].sciences or {})) then
             goto continue
         end
 
         -- Filter 2: Disabled/hidden tech
-        if filter.hide_tech["disabled_tech"] and (not ssftt.technology.enabled or ssftt.hidden) then
+        if filter.hide_tech["disabled_tech"] and meta[tech].is_disabled then
             goto continue
         end
 
         -- Filter 3: Manual trigger tech
-        if filter.hide_tech["manual_trigger_tech"] and ssftt.has_trigger then
+        if filter.hide_tech["manual_trigger_tech"] and meta[tech].is_blocking then
             goto continue
         end
 
         -- Filter 4: Infinite tech
-        if filter.hide_tech["infinite_tech"] and ssftt.is_infinite then
+        if filter.hide_tech["infinite_tech"] and meta[tech].is_infinite then
             goto continue
         end
 
         -- Filter 5: Inherited tech
-        if filter.hide_tech["inherited_tech"] and (ssftt.inherited_by ~= nil or ssftt.queued) then
+        if filter.hide_tech["inherited_tech"] and meta[tech].is_inherited then
             goto continue
         end
 
